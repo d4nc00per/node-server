740d4193e7b953f0a5b8e5d71ba46c4ee9c93f9c
Daniel Cooper <cooper.daniel45@gmail.com>
2015-08-04 22:36:42 +0100

    Added basic server with get and post homepage

---
+++ node-server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./static'));
+
+app.get("/", returnView);
+
+app.post('/index', login);
+
+app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
+});
+
+function login(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+
+	res.send(username + password)
+}
+
+function returnView(req, res) {
+	res.set('Content-Type', 'text/html');
+	res.send(new Buffer(fs.readFileSync("index.html")));
+}
